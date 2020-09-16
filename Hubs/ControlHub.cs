529a7e6600c2e7ede05ebe8105a4a9bc56d1569c
pockeviciuspovilas <47566999+pockeviciuspovilas@users.noreply.github.com>
2020-09-16 22:18:20 +0300

    Add project files.

---
+++ Hubs/ControlHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AZH_Tankai_Server.Hubs
+{
+    public class ControlHub : Hub
+    {
+        public Task SendCoordinate(string user, int x, int y)
+        {
+            
+            return Clients.All.SendAsync("ReceiveCoordinate", user, x, y);
+        }
+
+        public Task SendPlayer(string user)
+        {
+            return Clients.All.SendAsync("ReceiveUser", user);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using AZH_Tankai_Server.Hubs;
+using Microsoft.AspNetCore.Http;
+
+namespace AZH_Tankai_Server
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => false;
+                options.MinimumSameSitePolicy = Microsoft.AspNetCore.Http.SameSiteMode.None;
+            });       
+            services.AddSignalR();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<ControlHub>("/ControlHub");
+            });
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
