1224c715461d64fface7e5eade68952191ec9bf8
Liudvikas Sablauskas <liudvikas.sablauskas@telesoftas.com>
2020-10-27 20:44:32 +0200

    Add all the types of bullets with empty implementations

---
+++ Models/Bullets/Bullet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AZH_Tankai_Server.Models.Bullets
+{
+    abstract class Bullet
+    {
+        public int Velocity { get; set; }
+        public int Direction { get; set; }
+        public int SpawnTime { get; set; }
+    }
+}
---
+++ Models/Bullets/BulletFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AZH_Tankai_Server.Models.Bullets
+{
+    class BulletFactory
+    {
+        // TODO
+    }
+}
