42bdd8e78f731fe3350864157a53ad816e8860cd
Liudvikas Sablauskas <liudvikas.sablauskas@telesoftas.com>
2020-11-03 22:05:29 +0200

    Add basic BulletStorage singleton

---
+++ Models/Bullets/BulletStorage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AZH_Tankai_Server.Models.Bullets
+{
+    class BulletStorage
+    {
+        static object thisLock = new object();
+        private BulletStorage()
+        {
+            bullets = new List<Bullet>();
+        }
+
+        private static BulletStorage singleton;
+        public static BulletStorage Get()
+        {
+            lock (thisLock)
+            {
+                if (singleton == null)
+                {
+                    singleton = new BulletStorage();
+                }
+                return singleton;
+            }
+        }
+
+        readonly List<Bullet> bullets;
+
+        public void Add(Bullet bullet)
+        {
+            bullets.Add(bullet);
+        }
+    }
+}
