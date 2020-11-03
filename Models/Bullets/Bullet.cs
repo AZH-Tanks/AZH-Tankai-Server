882fab5e8a653d7e87299ba7aa50aea1ce49bde7
Liudvikas Sablauskas <liudvikas.sablauskas@telesoftas.com>
2020-11-03 23:41:52 +0200

    Add Type and Location to Bullet class

--- Models/Bullets/Bullet.cs
+++ Models/Bullets/Bullet.cs
@@ -7,6 +7,8 @@ namespace AZH_Tankai_Server.Models.Bullets
 {
     class Bullet
     {
+        public string Type { get; set; }
+        public Point Location { get; set; }
         public int Velocity { get; set; }
         public int Direction { get; set; }
         public long SpawnTime { get; set; }
