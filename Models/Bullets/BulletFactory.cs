1feb3365b470d699a410677a85462f367db9b95a
Liudvikas Sablauskas <liudvikas.sablauskas@telesoftas.com>
2020-11-03 23:42:54 +0200

    Update BulletFactory to allow cretae Laser, Shrapnel and BasicBullet

--- Models/Bullets/BulletFactory.cs
+++ Models/Bullets/BulletFactory.cs
@@ -6,13 +6,37 @@ namespace AZH_Tankai_Server.Models.Bullets
 {
     class BulletFactory
     {
-        public Bullet createBullet(string type)
-        { 
-            switch(type)
+        public Bullet createBullet(string type, int x, int y)
+        {
+            switch (type)
             {
-                case "Basic": return new BasicBullet();
-                case "Laser": return new Laser();
-                case "Shrapnel": return new Shrapnel();
+                case "Basic":
+                    {
+                        var bullet = new BasicBullet();
+                        bullet.Type = type;
+                        bullet.Location = new Point(x, y);
+                        bullet.Velocity = 5;
+                        bullet.SpawnTime = DateTime.Now.Ticks;
+                        return bullet;
+                    };
+                case "Laser":
+                    {
+                        var bullet = new Laser();
+                        bullet.Type = type;
+                        bullet.Location = new Point(x, y);
+                        bullet.Velocity = 15;
+                        bullet.SpawnTime = DateTime.Now.Ticks;
+                        return bullet;
+                    };
+                case "Shrapnel":
+                    {
+                        var bullet = new Shrapnel();
+                        bullet.Type = type;
+                        bullet.Location = new Point(x, y);
+                        bullet.Velocity = 10;
+                        bullet.SpawnTime = DateTime.Now.Ticks;
+                        return bullet;
+                    };
                 case "HomingMissile": return new HomingMissile();
                 case "Exploding": return new ExplodingBullet();
                 case "Machinegun": return new MachinegunBullet();
