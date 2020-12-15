a6ecfe3d431ed6276357c60d40a9279bfa164d4f
Mantas Grabauskas <mantelisas@gmail.com>
2020-12-15 22:37:13 +0200

    Add collision WIP

---
+++ AZH-Tankai-Server/Controllers/Collision/CollisionComponent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AZH_Tankai_Server.Controllers.Collision
+{
+    public class CollisionComponent : IEnumerable
+    {
+        private CollisionComponent[] components;
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AZH-Tankai-Server/Controllers/Collision/CollisionComponentEnumerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AZH_Tankai_Server.Controllers.Collision
+{
+    public class CollisionComponentEnumerator : IEnumerator
+    {
+        public object Current => throw new NotImplementedException();
+
+        public bool MoveNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
